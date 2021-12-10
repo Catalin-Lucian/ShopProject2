607f2f6a0371ef6fc9e24707814e57b803fc3243
Lucian <63058063+Catalin-Lucian@users.noreply.github.com>
2021-12-10 19:41:12 +0200

    added get to book from orderservice

--- ShopProject-Order/src/main/java/com/example/shopproject/Service/OrderService.java
+++ ShopProject-Order/src/main/java/com/example/shopproject/Service/OrderService.java
@@ -2,6 +2,7 @@ package com.example.shopproject.Service;
 
 import com.example.shopproject.Mapper.OrderMapper;
 import com.example.shopproject.Model.DTO.Order.PostOrderDTO;
+import com.example.shopproject.Model.Entity.Book.Book;
 import com.example.shopproject.Model.Entity.Order.Item;
 import com.example.shopproject.Model.Entity.Order.Order;
 import com.mongodb.client.MongoClients;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -18,15 +20,18 @@ public class OrderService {
     MongoOperations mongoOps = new MongoTemplate(new SimpleMongoClientDatabaseFactory(MongoClients.create(),
             "book-order"));
 
+    RestTemplate restTemplate = new RestTemplate();
 
     public void PostOrder(PostOrderDTO postOrderDTO, Integer idUser) {
+        String bookUri = "http://localhost:8080/api/bookcollection/books/";
         Order order = OrderMapper.convertToOrder(postOrderDTO);
         List<Item> items = order.getItems();
         for (Item item : items) {
-            // Book book = bookService.GetBookByIsbn(item.getIsbn());
+             Book book = restTemplate.getForObject(bookUri+item.getIsbn(),Book.class);
+             System.out.println(book);
 
         }
-        mongoOps.insert(order, "Client." + idUser);
+//        mongoOps.insert(order, "Client." + idUser);
     }
 
     public void DeleteAllOrders(Integer idUser) {
